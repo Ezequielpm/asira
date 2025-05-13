d63c55d4c2f2cbc6933d3ad42b480af9197851b6
Ezequiel <ezequielmazariegossolis@gmail.com>
2025-05-13 09:43:21 -0600

    Initial Commit

---
+++ AgroYouthAppApp/AgroYouthAppAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgroYouthAppAppApp.swift
+//  AgroYouthAppApp
+//
+//  Created by Ezequiel Peña on 13/05/25.
+//
+
+import SwiftUI
+
+@main
+struct AgroYouthAppAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AgroYouthAppAppTests/AgroYouthAppAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  AgroYouthAppAppTests.swift
+//  AgroYouthAppAppTests
+//
+//  Created by Ezequiel Peña on 13/05/25.
+//
+
+import Testing
+@testable import AgroYouthAppApp
+
+struct AgroYouthAppAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
